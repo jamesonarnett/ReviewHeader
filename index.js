9fb8cb00a2a47e96ac7c1424832cd16c45cac3af
Jameson <90803725+jamesonarnett@users.noreply.github.com>
2021-12-04 21:36:23 -0600

    Add "forms" Btn scroll to "review"

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-$(".container-graph")
-  .mouseover(function () {
-    $(".anchor").stop().fadeTo("fast", 0.6);
-  })
-  .mouseout(function () {
-    $(".anchor").stop().fadeTo("fast", 1);
+$(document).ready(function () {
+  $("#btn1").click(function () {
+    $("#form1").toggle();
   });
+  $("#btn2").click(function () {
+    $("#form2").toggle();
+  });
+  $(".container-graph")
+    .mouseover(function () {
+      $(".anchor").stop().fadeTo("fast", 0.6);
+    })
+    .mouseout(function () {
+      $(".anchor").stop().fadeTo("fast", 1);
+    });
+
+  $(".anchor").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#myDiv").offset().top,
+      },
+      500
+    );
+  });
+});
